476e365f73ca2a71fe28c33593b8d65aa3fad3c6
Your Name <you@example.com>
2025-06-19 11:03:36 +0530

    june 19 GOING TO AWS FINALY

--- src/main/java/com/prodev/spring/cloud/gateway/cloudgateway/component/LoggingGlobalPostFilter.java
+++ src/main/java/com/prodev/spring/cloud/gateway/cloudgateway/component/LoggingGlobalPostFilter.java
@@ -1,30 +1,30 @@
-package com.prodev.spring.cloud.gateway.cloudgateway.component;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
-
-@Component
-public class LoggingGlobalPostFilter {
-    final Logger logger =
-            LoggerFactory.getLogger(
-                    LoggingGlobalPostFilter.class);
-
-    @Bean
-    public GlobalFilter postGlobalFilter() {
-        return (exchange, chain) -> {
-            System.out.println("hey this is pre filter");
-            logger.info("hey this is pre filter  from logger");
-            return chain.filter(exchange)
-                    .then(Mono.fromRunnable(() -> {
-                        ServerHttpRequest req = exchange.getRequest();
-                        logger.info("Global Post Filter executed");
-                    }));
-        };
-    }
-}
-
+//package com.prodev.spring.cloud.gateway.cloudgateway.component;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.stereotype.Component;
+//import reactor.core.publisher.Mono;
+//
+//@Component
+//public class LoggingGlobalPostFilter {
+//    final Logger logger =
+//            LoggerFactory.getLogger(
+//                    LoggingGlobalPostFilter.class);
+//
+//    @Bean
+//    public GlobalFilter postGlobalFilter() {
+//        return (exchange, chain) -> {
+//            System.out.println("hey this is pre filter");
+//            logger.info("hey this is pre filter  from logger");
+//            return chain.filter(exchange)
+//                    .then(Mono.fromRunnable(() -> {
+//                        ServerHttpRequest req = exchange.getRequest();
+//                        logger.info("Global Post Filter executed");
+//                    }));
+//        };
+//    }
+//}
+//
--- src/main/java/com/prodev/spring/cloud/gateway/cloudgateway/component/LoggingGlobalPreFilter.java
+++ src/main/java/com/prodev/spring/cloud/gateway/cloudgateway/component/LoggingGlobalPreFilter.java
@@ -1,38 +1,38 @@
-package com.prodev.spring.cloud.gateway.cloudgateway.component;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-
-@Component
-public class LoggingGlobalPreFilter implements GlobalFilter {
-
-    final Logger logger =
-            LoggerFactory.getLogger(LoggingGlobalPreFilter.class);
-
-    @Override
-    public Mono<Void> filter(
-            ServerWebExchange exchange,
-            GatewayFilterChain chain) {
-        System.out.println(" coming with request....");
-        ServerHttpRequest req=exchange.getRequest();
-
-        if(req.getMethod()== HttpMethod.GET && !req.getHeaders().containsKey("Authorization")){
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            // Optionally, write an error message to the response body
-            // exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap("Error: Missing header".getBytes())));
-            exchange.getResponse().setComplete();
-        }
-        logger.info("Global Pre Filter executed");
-        return chain.filter(exchange);
-    }
-}
+//package com.prodev.spring.cloud.gateway.cloudgateway.component;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//
+//@Component
+//public class LoggingGlobalPreFilter implements GlobalFilter {
+//
+//    final Logger logger =
+//            LoggerFactory.getLogger(LoggingGlobalPreFilter.class);
+//
+//    @Override
+//    public Mono<Void> filter(
+//            ServerWebExchange exchange,
+//            GatewayFilterChain chain) {
+//        System.out.println(" coming with request....");
+//        ServerHttpRequest req=exchange.getRequest();
+//
+//        if(req.getMethod()== HttpMethod.GET && !req.getHeaders().containsKey("Authorization")){
+//            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//            // Optionally, write an error message to the response body
+//            // exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap("Error: Missing header".getBytes())));
+//            exchange.getResponse().setComplete();
+//        }
+//        logger.info("Global Pre Filter executed");
+//        return chain.filter(exchange);
+//    }
+//}
